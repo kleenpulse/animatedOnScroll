f72e9bd5c151dd1c8f73cabde54b5ca478510477
Isaac Gideon <skyeparagon@gmail.com>
2022-09-15 14:33:10 +0100

    AnimatedOnScroll

---
+++ app.js
@@ -0,0 +1,28 @@
+const sayHelloEl = document.getElementById('say-hello')
+const hiddenEl = document.querySelectorAll('.hidden')
+const shadowEl = document.querySelector('.logos')
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            if (entry.target === shadowEl) {
+                entry.target.classList.add('show', 'shadow')
+            } else { entry.target.classList.add('show') }
+        } else { entry.target.classList.remove('show', 'shadow') }
+    })
+})
+hiddenEl.forEach((el) => observer.observe(el))
+// Say Hello
+function sayHello() {
+    let welcome;
+    let date = new Date();
+    let helloHour = date.getHours();
+    if (helloHour < 12) {
+        welcome = "Good Morning";
+    } else if (helloHour < 16) {
+        welcome = "Good Afternoon";
+    } else {
+        welcome = "Good Evening";
+    }
+    return welcome
+}
+sayHelloEl.textContent = sayHello()
